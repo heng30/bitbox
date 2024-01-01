1aa81c6d930f331282a559540af8aa5102b9bab7
blueJpg <2238288979@qq.com>
2024-01-01 21:08:48 +0800

    [*] improve ui

--- bitbox/src/logic/setting.rs
+++ bitbox/src/logic/setting.rs
@@ -43,8 +43,8 @@ pub fn init(ui: &AppWindow) {
                 .win_width
                 .to_string()
                 .parse()
-                .unwrap_or(700),
-            700,
+                .unwrap_or(800),
+            800,
         );
         config.ui.win_height = u32::max(
             setting_config
@@ -112,7 +112,7 @@ fn init_setting_dialog(ui: Weak<AppWindow>) {
     let mut setting_dialog = ui.global::<Store>().get_setting_dialog_config();
     setting_dialog.ui.font_size = slint::format!("{}", ui_config.font_size);
     setting_dialog.ui.font_family = ui_config.font_family.into();
-    setting_dialog.ui.win_width = slint::format!("{}", u32::max(ui_config.win_width, 700));
+    setting_dialog.ui.win_width = slint::format!("{}", u32::max(ui_config.win_width, 800));
     setting_dialog.ui.win_height = slint::format!("{}", u32::max(ui_config.win_height, 600));
     setting_dialog.ui.language = ui_config.language.into();
 
