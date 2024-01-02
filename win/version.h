6f5efa5761453b9f7086c4070f6524baedde7e1d
blueJpg <2238288979@qq.com>
2024-01-02 14:34:52 +0800

    [*] imporve UI

--- win/version.h
+++ win/version.h
@@ -10,7 +10,7 @@
 #define PRODUCT_VERSION_STR    "0.1.0"
 #define COMPANY_NAME           "Heng30"
 #define INTERNAL_NAME          "bitbox.exe"
-#define FILE_DESCRIPTION       "bitbox for local network communication"  // 文件说明
+#define FILE_DESCRIPTION       "bitbox is a light bitoin wallet"  // 文件说明
 #define LEGAL_COPYRIGHT        "Copyright 2023-2033 The Heng30 Company Ltd. All rights reserved." // 版权
 #define ORIGINAL_FILE_NAME     "bitbox.exe"    // 原始文件名
 #define ORGANIZATION_DOMAIN    "https://heng30.xyz"  // 域名
